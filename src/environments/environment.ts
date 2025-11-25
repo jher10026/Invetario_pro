5df400884d91853760e65d261413e94c74de3963
chatbotsumaq-cloud <chatbotsumaq@gmail.com>
2025-11-25 15:03:06 -0500

    subir a la rama firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "TU_API_KEY_AQUI",
+    authDomain: "TU_PROJECT_ID.firebaseapp.com",
+    projectId: "TU_PROJECT_ID",
+    storageBucket: "TU_PROJECT_ID.appspot.com",
+    messagingSenderId: "TU_SENDER_ID",
+    appId: "TU_APP_ID"
+  }
+};
